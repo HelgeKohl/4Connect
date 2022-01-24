ae2d7167ac9f23f304084b813d681ac574749054
Prettner <s.prettner@oth-aw.de>
2022-01-24 15:20:17 +0100

    Unity Projekt mit ObjectDetection.cs

---
+++ 4-connect-ar/Assets/Scripts/BoardDetection.cs
@@ -0,0 +1,57 @@
+using OpenCvSharp;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoardDetection : MonoBehaviour
+{
+    public RawImage background; // Theorie: Nur das Cam-Bild
+    public RawImage overlay; // Theorie: Hier nur die Rechtecke zeichnen, ohne Cam-Bild
+    public int Width = 640;
+    public int Height = 480;
+
+    ObjectDetection objectDetection;
+    CustomCamera camera;
+
+    // Debug
+    public bool debug;
+    public bool debugFps;
+    System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+
+    void Start()
+    {
+        // https://www.nuget.org/packages/OpenCvSharp4/
+        // https://www.tech-quantum.com/have-fun-with-webcam-and-opencv-in-csharp-part-1/
+        // https://www.tech-quantum.com/have-fun-with-webcam-and-opencv-in-csharp-part-2/
+
+        Screen.SetResolution(Width, Height, FullScreenMode.Windowed);
+        camera = new CustomCamera(background, Width, Height);
+        objectDetection = new ObjectDetection();
+    }
+
+    private void Update()
+    {   
+
+    }
+
+    private void FixedUpdate()
+    {
+        // Time
+        stopwatch.Restart();
+        camera.Refresh();
+
+        // Objekterkennung durchführen
+        Mat mat = camera.GetCurrentFrameAsMat();
+        Texture2D texture = objectDetection.DetectObjects(mat);
+        background.texture = texture;
+        // ---
+
+        stopwatch.Stop();
+        if (debugFps && stopwatch.ElapsedMilliseconds != 0)
+        {
+            Debug.Log(stopwatch.ElapsedMilliseconds);
+        }
+    }
+}
---
+++ 4-connect-ar/Assets/Scripts/ObjectDetection.cs
@@ -0,0 +1,90 @@
+using OpenCvSharp;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDetection : MonoBehaviour
+{
+    CascadeClassifier board_haar_cascade;
+    List<OpenCvSharp.Rect> boardBounds = new List<OpenCvSharp.Rect>();
+    Texture2D currentOverlay;
+
+    public ObjectDetection()
+    {
+        //Define the face and eyes classifies using Haar-cascade xml
+        //Download location: https://github.com/opencv/opencv/tree/master/data/haarcascades
+        //
+        // TODO: Dafür wird noch eine Lösung gebraucht ...
+        //board_haar_cascade = new CascadeClassifier(@"C:\Development\arvr-projekt\data\images\cascade\cascade.xml");
+        board_haar_cascade = new CascadeClassifier(@"https://cdn.discordapp.com/attachments/933311227550138419/935176953642766356/cascade.xml");
+    }
+
+    public Texture2D DetectObjects(Mat image)
+    {
+        if (image == null)
+        {
+            return null;
+        }
+
+        // Convert to gray scale to improve the image processing
+        Mat gray = ConvertGrayScale(image);
+
+        // Detect boards using Cascase classifier
+        OpenCvSharp.Rect[] boards = DetectBoards(gray);
+        if (image.Empty())
+            return null;
+
+        // Alte Position löschen
+        boardBounds.Clear();
+
+        // Loop through detected boards
+        foreach (var item in boards)
+        {
+            boardBounds.Add(item);
+        }
+
+        // Mark the detected board on the original frame
+        MarkFeatures(image);
+
+        OpenCvHelper.Overlay = OpenCvSharp.Unity.MatToTexture(image);
+        
+        return currentOverlay;
+    }
+
+    private Mat ConvertGrayScale(Mat image)
+    {
+        Mat gray = new Mat();
+        Cv2.CvtColor(image, gray, ColorConversionCodes.BGR2GRAY);
+        return gray;
+    }
+
+    private OpenCvSharp.Rect[] DetectBoards(Mat image)
+    {
+        // Parameter zum Anpassen
+        OpenCvSharp.Rect[] boardFeature = board_haar_cascade.DetectMultiScale(image, 1.3, 50);
+        return boardFeature;
+    }
+
+    private void MarkFeatures(Mat image)
+    {
+        foreach (OpenCvSharp.Rect bounds in boardBounds)
+        {
+            OpenCvSharp.Rect biggerRect = new OpenCvSharp.Rect();
+            double scaleXby = 1.6;
+            double scaleYby = 1.2;
+            biggerRect.X = bounds.X - (int)(bounds.Width * ((scaleXby - 1) / 2));
+            biggerRect.Y = bounds.Y - (int)(bounds.Height * ((scaleYby - 1) / 2));
+            biggerRect.Width = (int)(bounds.Width * scaleXby);
+            biggerRect.Height = (int)(bounds.Height * scaleYby);
+
+            biggerRect.X = biggerRect.X < 0 ? 0 : biggerRect.X;
+            biggerRect.Y = biggerRect.Y < 0 ? 0 : biggerRect.Y;
+            biggerRect.Width = biggerRect.X + biggerRect.Width > Screen.width ? Screen.width : biggerRect.Width;
+            biggerRect.Height = biggerRect.Y + biggerRect.Height > Screen.height ? Screen.height : biggerRect.Height;
+
+            Cv2.Rectangle(image, bounds, new Scalar(0, 255, 0), thickness: 5);
+
+            Cv2.Rectangle(image, biggerRect, new Scalar(255, 255, 0), thickness: 5);
+        }
+    }
+}
